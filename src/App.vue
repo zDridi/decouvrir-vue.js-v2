bbca9a4a261244f62ca53da667ea3e9c637cb85e
zDridi <dridi.zied@courrier.uqam.ca>
2021-12-12 21:18:16 -0500

    Ajout d'un nouveau composant 'score'

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <game></game>
+    <score></score>
   </div>
 </template>
 
 <script>
 import Game from './components/Game'
+import Score from './components/Score'
 
 export default {
   name: 'App',
   components: {
-    Game
+    Game,
+    Score
   }
 }
 </script>
---
+++ src/components/Score.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="score">
+    Votre score  : 0
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'score'
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
