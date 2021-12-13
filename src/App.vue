838bea685591a7c1cb99916f1cc09bb761adae73
zDridi <dridi.zied@courrier.uqam.ca>
2021-12-12 20:59:50 -0500

    Modification du projet de base (Hello par Game)

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+
+    '<game></game>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Game from './components/Game'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Game
   }
 }
 </script>
@@ -21,8 +21,5 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
