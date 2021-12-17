c1f4320c9b54147bb2f3883267d17deb5795ed12
zDridi <dridi.zied@courrier.uqam.ca>
2021-12-16 22:40:01 -0500

    Exmple de communication grâce aux attributs et exemple d'utilisation des directives

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="player">
-    {{ welcomeMessage }}
+  <div>
+    <span v-hide>
+      {{ welcomeMessage }}
+    </span>
+    <form v-hide>
+      <input name="player" placeholder="Entrez votre nom de joueur" v-border:red/>
+      <button type="submit" v-border:yellow>Jouer</button>
+    </form>
   </div>
 </template>
 
@@ -8,8 +14,27 @@
 export default {
   name: 'player',
   created: function () {
-    this.player = 'Ronaldo'
-    this.welcomeMessage = this.player ? 'Bonjour ' + this.player + ' !' : 'Pas de joueur.'
+    this.player = ''
+    this.welcomeMessage = this.player ? 'Bonjour ' + this.player + ' !' : 'Pas de joueur'
+  },
+  directives: {
+    border: function (el, binding) {
+      el.style.borderColor = binding.arg
+    },
+    hide: function (el, binding, vnode) {
+      let isForm = vnode.tag === 'form'
+      console.log(isForm)
+      let isSpan = vnode.tag === 'span'
+      console.log(isSpan)
+      let player = vnode.context.player
+      console.log(player)
+
+      if (isForm) {
+        el.style.diplay = player ? 'none' : 'block'
+      } else if (isSpan) {
+        el.style.display = player ? 'block' : 'none'
+      }
+    }
   }
 
 }
